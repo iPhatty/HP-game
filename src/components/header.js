12593a61d2a6cee734745da64476a461c89ea0c4
Phat Ma <phat.ma@hotmail.co.uk>
2018-05-23 16:17:49 +0100

    merge with master

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Game from "./game/game";
 
+
 export const Header = props => {
-  const { avatar_url, html_url, name, followers, repos_url, login } = props.data;
+  const { avatar_url, html_url, login } = props.data;
   return (
     <div className="card">
       <h1 data-testid="userData">Hello <a href={html_url}>{login}!</a></h1>
